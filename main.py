499883007d672a5f61a99ad5fca61d9ea0d8bb8c
emeijdam <emeijdam@dasc.nl>
2024-04-04 09:48:13 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import json
+from pprint import pprint 
+import pandas as pd
+
+api_url = "https://gateway.apiportal.ns.nl/nsapp-stations/v2"
+headers =  {"Cache-Control": "no-cache","Ocp-Apim-Subscription-Key": "30db5fa047c3450ca3e131452dfdc81e"}
+#todo = {"userId": 1, "title": "Buy milk", "completed": False}
+#response = requests.get(api_url)
+response = requests.get(api_url, headers=headers)
+data = response.json()
+payload = data['payload']
+#pprint(data['payload'])
+
+
+for line in payload:
+    print(type(line))
+    print(line)
+
+print(type(payload))
+
+df = pd.DataFrame(payload).explode('namen', 'synoniemen')
+
+print(df.head)
+
+df.to_csv("file_name2.csv", encoding='utf-8', index=False)
